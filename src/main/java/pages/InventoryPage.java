b4928805d7cc3b7e9d6e0902e4f9c8261c9776b6
Yara Shaker <Yara.shaker99@gmail.com>
2026-02-23 08:45:56 +0200

    Compelet Part 2

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -1,34 +1,3 @@
-/*package pages;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
-public class InventoryPage {
-
-    WebDriver driver;
-
-    public InventoryPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    By pageTitle = By.className("title");
-    By cartIcon = By.id("shopping_cart_container");
-    By products = By.className("inventory_item");
-
-    public String getPageTitle() {
-        return driver.findElement(pageTitle).getText();
-    }
-
-    public boolean isCartIconDisplayed() {
-        return driver.findElement(cartIcon).isDisplayed();
-    }
-
-    public int getProductsCount() {
-        return driver.findElements(products).size();
-    }
-}
-
- */
 package pages;
 
 import org.openqa.selenium.By;
@@ -49,6 +18,12 @@ public class InventoryPage {
     By cartIcon = By.id("shopping_cart_container");
     By products = By.className("inventory_item");
 
+
+    By linkedinIcon = By.cssSelector("a[href*='linkedin']");
+    By facebookIcon = By.cssSelector("a[href*='facebook']");
+    By twitterIcon = By.cssSelector("a[href*='twitter']");
+
+
     public String getPageTitle() {
         return driver.findElement(pageTitle).getText();
     }
@@ -60,5 +35,78 @@ public class InventoryPage {
     public int getProductCount() {
         List<WebElement> productList = driver.findElements(products);
         return productList.size();
+    }
+
+    public void clickLinkedInIcon() {
+        driver.findElement(linkedinIcon).click();
+    }
+
+    public void clickFacebookIcon() {
+        driver.findElement(facebookIcon).click();
+    }
+
+    public void clickTwitterIcon() {
+        driver.findElement(twitterIcon).click();
+    }
+    By cartItems = By.className("cart_item");
+    public boolean isCartEmpty() {
+        List<WebElement> items = driver.findElements(cartItems);
+        return items.size() == 0;
+    }
+    public void clickCartIcon() {
+        driver.findElement(cartIcon).click();
+    }
+    public void addProductToCart(String productName) {
+        List<WebElement> allProducts = driver.findElements(products);
+        for (WebElement product : allProducts) {
+            String name = product.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                product.findElement(By.tagName("button")).click();
+                break;
+            }
+        }
+    }
+    public boolean isProductInCart(String productName) {
+        List<WebElement> cartItems = driver.findElements(By.className("cart_item"));
+        for (WebElement item : cartItems) {
+            String name = item.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeProductFromCart(String productName) {
+        List<WebElement> cartItems = driver.findElements(By.className("cart_item"));
+        for (WebElement item : cartItems) {
+            String name = item.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                item.findElement(By.tagName("button")).click();
+                break;
+            }
+        }
+    }
+    public boolean isAddToCartButtonVisible(String productName) {
+        List<WebElement> allProducts = driver.findElements(products);
+        for (WebElement product : allProducts) {
+            String name = product.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                String btnText = product.findElement(By.tagName("button")).getText();
+                return btnText.equalsIgnoreCase("Add to cart");
+            }
+        }
+        return false;
+    }
+    public boolean isRemoveButtonVisible(String productName) {
+        List<WebElement> allProducts = driver.findElements(products);
+        for (WebElement product : allProducts) {
+            String name = product.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                String btnText = product.findElement(By.tagName("button")).getText();
+                return btnText.equalsIgnoreCase("Remove");
+            }
+        }
+        return false;
     }
 }
--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,4 +1,98 @@
 package tests;
 
-public class CartTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.InventoryPage;
+import base.BaseTest;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class CartTest extends BaseTest {
+
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
+
+    @Test
+    public void verifyLinkedInLink() {
+
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.clickLinkedInIcon();
+        for(String handle : driver.getWindowHandles()){
+            driver.switchTo().window(handle);
+        }
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(d -> d.getCurrentUrl().contains("linkedin"));
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("linkedin"));
+    }
+    @Test
+    public void verifyFacebookLink() {
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.clickFacebookIcon();
+        for(String handle : driver.getWindowHandles()){
+            driver.switchTo().window(handle);
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(d -> d.getCurrentUrl().contains("facebook"));
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("facebook"));
+    }
+    @Test
+    public void verifyTwitterLink() {
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.clickTwitterIcon();
+        for(String handle : driver.getWindowHandles()){
+            driver.switchTo().window(handle);
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(d -> d.getCurrentUrl().contains("x.com"));
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("x.com"));
+    }
+    @Test
+    public void verifyCartIsEmpty() {
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.clickCartIcon();
+        Assert.assertTrue(inventoryPage.isCartEmpty());
+    }
+    @Test
+    public void addThreeProductsAndVerifyInCart() {
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.addProductToCart("Sauce Labs Backpack");
+        inventoryPage.addProductToCart("Sauce Labs Bolt T-Shirt");
+        inventoryPage.addProductToCart("Sauce Labs Onesie");
+        inventoryPage.clickCartIcon();
+        Assert.assertTrue(inventoryPage.isProductInCart("Sauce Labs Backpack"));
+        Assert.assertTrue(inventoryPage.isProductInCart("Sauce Labs Bolt T-Shirt"));
+        Assert.assertTrue(inventoryPage.isProductInCart("Sauce Labs Onesie"));
+    }
+    @Test
+    public void removeOneProductAndVerify() {
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        inventoryPage.addProductToCart("Sauce Labs Backpack");
+        inventoryPage.addProductToCart("Sauce Labs Bolt T-Shirt");
+        inventoryPage.addProductToCart("Sauce Labs Onesie");
+        inventoryPage.clickCartIcon();
+        inventoryPage.removeProductFromCart("Sauce Labs Bolt T-Shirt");
+        driver.navigate().back();
+        Assert.assertTrue(inventoryPage.isAddToCartButtonVisible("Sauce Labs Bolt T-Shirt"));
+        Assert.assertTrue(inventoryPage.isRemoveButtonVisible("Sauce Labs Backpack"));
+        Assert.assertTrue(inventoryPage.isRemoveButtonVisible("Sauce Labs Onesie"));
+    }
 }
